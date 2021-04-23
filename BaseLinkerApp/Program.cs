a820cb047bb6507736c9df919ed2973cb362d06e
Vesker7 <246658@student.pwr.edu.pl>
2021-04-23 22:01:23 +0200

    Basic Working WebClient

--- BaseLinkerApp/Program.cs
+++ BaseLinkerApp/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+using System.Text;
 
 namespace BaseLinkerApp
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string metoda = "getOrders";
+            string token = "3000897-3004070-XLT2A2AZ8MDQVN1PYL2N1QVNC7W753MFLEKIHYD32CIYJDES7QH33EN88GZ0IINS";
+            string address = "https://api.baselinker.com/connector.php";
+
+            WebClient client = new WebClient();
+
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+            client.QueryString.Add("token", token);
+            client.QueryString.Add("method", metoda);
+
+            var data = client.UploadValues(address, "POST", client.QueryString);
+            JObject reponse = JObject.Parse(Encoding.UTF8.GetString(data));
+
+            Console.WriteLine(reponse);
+
         }
     }
 }
