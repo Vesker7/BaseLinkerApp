fec926cb3fc1f0e238a9308a42a92106bfce32bd
Vesker7 <246658@student.pwr.edu.pl>
2021-04-24 05:14:47 +0200

    Functional copying of orders and adding notes

--- BaseLinkerApp/BaseLinkerClient.cs
+++ BaseLinkerApp/BaseLinkerClient.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
+using System.Text;
 using Newtonsoft.Json.Linq;
 
 namespace BaseLinkerApp
@@ -15,15 +15,150 @@ namespace BaseLinkerApp
         {
             Token = token;
             _logService = logsOutput;
-        }
-
-        private void ApplyRequestBody(string method)
-        {
             client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
             client.QueryString.Add("token", Token);
-            client.QueryString.Add("method", method);
+            client.QueryString.Add("method", "");
+            client.QueryString.Add("parameters", "");
         }
 
-        // TODO: ADD API METHODS: getProducts, addProduct, getOrders, getOrder, addOrder, 
+        private void ChooseMethod(string method)
+        {
+            client.QueryString.Set("method", method);
+        }
+
+        public JObject GetOrders()
+        {
+            _logService.Print("Pobieranie zamówień.");
+            ChooseMethod("getOrders");
+
+            var data = client.UploadValues(Address, "POST", client.QueryString);
+            JObject response = JObject.Parse(Encoding.UTF8.GetString(data));
+
+            return response;
+        }
+
+        public JObject GetOrder(int order_id)
+        {
+            JObject param = new JObject();
+            param.Add("order_id", order_id);
+
+            _logService.Print("Pobieranie zamówienia o numerze: " + order_id + ".");
+            ChooseMethod("getOrders");
+            client.QueryString.Set("parameters", param.ToString());
+
+            var data = client.UploadValues(Address, "POST", client.QueryString);
+            JObject response = JObject.Parse(Encoding.UTF8.GetString(data));
+
+            return response;
+        }
+
+        public JObject GetStoragesList()
+        {
+            _logService.Print("Pobieranie dostępnych magazynów.");
+            ChooseMethod("getStoragesList");
+
+            var data = client.UploadValues(Address, "POST", client.QueryString);
+            JObject response = JObject.Parse(Encoding.UTF8.GetString(data));
+
+            return response;
+        }
+
+        public JObject GetCategories(string storage_id)
+        {
+            JObject param = new JObject();
+            param.Add("storage_id", storage_id);
+
+            _logService.Print("Pobieranie kategorii z magazynu: " + storage_id + ".");
+            ChooseMethod("getCategories");
+            client.QueryString.Set("parameters", param.ToString());
+
+            var data = client.UploadValues(Address, "POST", client.QueryString);
+            JObject response = JObject.Parse(Encoding.UTF8.GetString(data));
+
+            return response;
+        }
+
+        public JObject GetProductsList(string storage_id)
+        {
+            JObject param = new JObject();
+            param.Add("storage_id", storage_id);
+
+            _logService.Print("Pobieranie produktów z magazynu: " + storage_id + ".");
+            ChooseMethod("getProductsList");
+            client.QueryString.Set("parameters", param.ToString());
+
+            var data = client.UploadValues(Address, "POST", client.QueryString);
+            JObject response = JObject.Parse(Encoding.UTF8.GetString(data));
+
+            return response;
+        }
+
+        public JObject GetProduct(string storage_id, string filter_name)
+        {
+            JObject param = new JObject();
+            param.Add("storage_id", storage_id);
+            param.Add("filter_name", filter_name);
+
+            _logService.Print("Pobieranie produktu o nazwie: " + filter_name + " z magazynu: " + storage_id + ".");
+            ChooseMethod("getProductsList");
+            client.QueryString.Set("parameters", param.ToString());
+
+            var data = client.UploadValues(Address, "POST", client.QueryString);
+            JObject response = JObject.Parse(Encoding.UTF8.GetString(data));
+
+            return response;
+        }
+
+        public JObject AddProduct(string storage_id, string category_id, string productName, int stockQuantity, float priceBrutto, int taxRate)
+        {
+            JObject param = new JObject();
+            param.Add("storage_id", storage_id);
+            param.Add("name", productName);
+            param.Add("category_id", category_id);
+            param.Add("quantity", stockQuantity);
+            param.Add("price_brutto", priceBrutto);
+            param.Add("tax_rate", taxRate);
+
+            _logService.Print("Dodawanie produktu o nazwie: " + productName + " stan magazynowy: " + stockQuantity + " cena brutto: " + priceBrutto + " VAT: " + taxRate + "%.");
+            ChooseMethod("addProduct");
+            client.QueryString.Set("parameters", param.ToString());
+
+            var data = client.UploadValues(Address, "POST", client.QueryString);
+            JObject response = JObject.Parse(Encoding.UTF8.GetString(data));
+
+            return response;
+        }
+
+        public JObject AddOrderWithGift(int order_id)
+        {
+            JObject order = GetOrder(order_id);
+
+            if(!order["orders"].HasValues)
+            {
+                _logService.Print("Tworzenie zamówienia na podstawie zamówienia nr: " + order_id + " nie powiodło się, gdyż nie istnieje zamówienie o takim numerze!");
+                return new JObject();
+            }
+
+            string[] paramsToDelete = { "order_id", "shop_order_id", "external_order_id", "order_source", "order_source_id", "order_source_info", "confirmed", "date_confirmed", "date_in_status",
+                                        "payment_done", "delivery_package_module", "delivery_package_nr", "order_page", "pick_state", "pack_state", "delivery_country", "invoice_country" };
+
+            foreach (string param in paramsToDelete)
+            {
+                order["orders"].First[param].Parent.Remove();
+            }
+
+            order["orders"].First["admin_comments"] = "Zamówienie utworzone na podstawie " + order_id;
+
+            // TODO: DODAĆ GRATIS
+
+            _logService.Print("Tworzenie zamówienia na podstawie zamówienia nr: " + order_id + ".");
+            ChooseMethod("addOrder");
+            client.QueryString.Set("parameters", order["orders"].First.ToString());
+
+            var data = client.UploadValues(Address, "POST", client.QueryString);
+            JObject response = JObject.Parse(Encoding.UTF8.GetString(data));
+
+            return response;
+        }
     }
 }
--- BaseLinkerApp/Program.cs
+++ BaseLinkerApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Text;
 using Newtonsoft.Json.Linq;
+using Ninject;
 
 namespace BaseLinkerApp
 {
@@ -9,15 +10,31 @@ namespace BaseLinkerApp
     {
         public static void Main(string[] args)
         {
-            BaseLinkerClient client = new("3000897-3004070-XLT2A2AZ8MDQVN1PYL2N1QVNC7W753MFLEKIHYD32CIYJDES7QH33EN88GZ0IINS", new FileLogs("appLogs.txt"));
+            ILogsService _logsService;
+            Console.WriteLine("Wybierz miejsce wypisywania logów (0 - konsola, 1 - plik): ");
+            var choice = Console.ReadLine();
 
-            /*
-            var data = client.UploadValues(address, "POST", client.QueryString);
-            JObject reponse = JObject.Parse(Encoding.UTF8.GetString(data));
+            if (choice == "1")
+            {
+                Console.WriteLine("Wybrano wypisywanie logów do pliku.");
+                Console.WriteLine("Podaj ścieżkę do pliku:");
+                string path = Console.ReadLine();
+                _logsService = new FileLogs("appLogs.txt");
+            }
+            else if (choice == "0")
+            {
+                Console.WriteLine("Wybrano wypisywanie logów w konsoli.");
+                _logsService = new ConsoleLogs();
+            }
+            else
+            {
+                Console.WriteLine("Błędny wybór! Domyślne ustawienie: Wypisywanie logów w konsoli.");
+                _logsService = new ConsoleLogs();
+            }
+           
+            BaseLinkerClient client = new("3000897-3004070-XLT2A2AZ8MDQVN1PYL2N1QVNC7W753MFLEKIHYD32CIYJDES7QH33EN88GZ0IINS", _logsService);
 
-            Console.WriteLine(reponse);
-            */
-
+            Console.WriteLine(client.AddOrderWithGift(4839622));
         }
     }
 }
